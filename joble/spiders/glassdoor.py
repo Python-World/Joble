f3885f7f3bf4d4ca474fe94622d122539696e2e6
Rishi N Desai <rishindesai@yahoo.com>
2020-10-01 20:58:49 +0530

    stored url

--- joble/spiders/glassdoor.py
+++ joble/spiders/glassdoor.py
@@ -6,13 +6,14 @@ import urllib
 class GlassdoorSpider(scrapy.Spider):
     name = 'Glassdoor'
     allowed_domains = ['glassdoor.com']
+    url = 'https://www.glassdoor.com'
 
     def __init__(self, keyword=None, count=20):
         self.keyword = keyword
         self.count = int(count)
 
     def start_requests(self):
-        url = 'https://www.glassdoor.com/Job/jobs.htm?sc.keyword={}'.format(self.keyword)
+        url = '{}/Job/jobs.htm?sc.keyword={}'.format(self.url, self.keyword)
         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
@@ -22,7 +23,7 @@ class GlassdoorSpider(scrapy.Spider):
             job = {
                 'title': element.attrib['data-normalize-job-title'],
                 'location': element.attrib['data-job-loc'],
-                'employer': 'https://www.glassdoor.com' + element.css('div div.jobHeader a span::text').get(),
+                'employer': self.url + element.css('div div.jobHeader a span::text').get(),
                 'job-link': element.css('div div.jobHeader a::attr(href)').get()
             }
             yield job
